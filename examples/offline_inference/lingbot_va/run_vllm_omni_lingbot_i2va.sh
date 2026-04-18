016dfa0429c2b257b9ee30e9ef4c4d18aa63badd
kevinzeng <2538015266@qq.com>
2026-04-18 22:31:40 +0800

    add cfg example

--- examples/offline_inference/lingbot_va/run_vllm_omni_lingbot_i2va.sh
+++ examples/offline_inference/lingbot_va/run_vllm_omni_lingbot_i2va.sh
@@ -5,6 +5,7 @@ MODEL_PATH=${MODEL_PATH:-robbyant/lingbot-va-posttrain-robotwin}
 OBS_DIR=${OBS_DIR:-robotwin_obs}
 PROMPT_FILE=${PROMPT_FILE:-robotwin_prompt.txt}
 NUM_CHUNKS=${NUM_CHUNKS:-10}
+CFG_PARALLEL_SIZE=${CFG_PARALLEL_SIZE:-1}
 SEED=${SEED:-42}
 OUT_VIDEO=${OUT_VIDEO:-vllm_robotwin_i2va.mp4}
 OUT_ACTIONS=${OUT_ACTIONS:-vllm_robotwin_i2va_actions.npy}
@@ -19,6 +20,7 @@ python end2end.py \
   --obs-dir "$OBS_DIR" \
   "${PROMPT_ARGS[@]}" \
   --num-chunks "$NUM_CHUNKS" \
+  --cfg-parallel-size "$CFG_PARALLEL_SIZE" \
   --seed "$SEED" \
   --output-video "$OUT_VIDEO" \
   --output-actions "$OUT_ACTIONS"
